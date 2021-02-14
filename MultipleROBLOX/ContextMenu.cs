ae4199e8c5db6564740d842b5e2d8cb18ff90b7a
Alex <58864658+AlexR32@users.noreply.github.com>
2021-02-14 18:24:24 +0300

    MultipleROBLOX Release

---
+++ MultipleROBLOX/ContextMenu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MultipleROBLOX
+{
+	class ContextMenu
+	{
+		public ContextMenuStrip Create()
+		{
+			ContextMenuStrip Menu = new ContextMenuStrip();
+			ToolStripMenuItem Item;
+			ToolStripSeparator Separator;
+
+			Item = new ToolStripMenuItem();
+			Item.Text = "Multiple ROBLOX";
+			Item.Enabled = false;
+			Menu.Items.Add(Item);
+
+			Separator = new ToolStripSeparator();
+			Menu.Items.Add(Separator);
+
+			Item = new ToolStripMenuItem();
+			Item.Text = "Close";
+			Item.Click += new System.EventHandler(ExitClick);
+			Menu.Items.Add(Item);
+			
+			new Mutex(true, "ROBLOX_singletonMutex");
+			return Menu;
+		}
+
+		void ExitClick(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+	}
+}
---
+++ MultipleROBLOX/ProcessIcon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using MultipleROBLOX.Properties;
+
+namespace MultipleROBLOX
+{
+	class ProcessIcon : IDisposable
+	{
+		NotifyIcon NI;
+
+		public ProcessIcon()
+		{
+			NI = new NotifyIcon();
+		}
+
+		public void Display()
+		{
+			NI.ContextMenuStrip = new ContextMenu().Create();
+			NI.Icon = Resources.ProgramIcon;
+			NI.Text = "Multiple ROBLOX";
+			NI.Visible = true;
+		}
+
+		public void Dispose()
+		{
+			NI.Dispose();
+		}
+	}
+}
