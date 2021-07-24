bccb84aba58dc028a5d0ce658169de06369d124b
AlexR32 <58864658+AlexR32@users.noreply.github.com>
2021-07-24 12:36:52 +0300

    Update BracketV3.lua

--- BracketV3.lua
+++ BracketV3.lua
@@ -359,7 +359,7 @@ function Library:CreateWindow(Config, Parent)
 				end
 				return ToggleInit
 			end
-			function SectionInit:CreateSlider(Name, Min, Max, Def, Callback)
+			function SectionInit:CreateSlider(Name, Min, Max, Precise, Callback)
 				local SliderInit = {}
 				local Slider = Folder.Slider:Clone()
 				Slider.Name = Name .. " S"
@@ -378,7 +378,7 @@ function Library:CreateWindow(Config, Parent)
 				local function Sliding(Input)
                     local Position = UDim2.new(math.clamp((Input.Position.X - Slider.Slider.AbsolutePosition.X) / Slider.Slider.AbsoluteSize.X,0,1),0,1,0)
                     Slider.Slider.Bar.Size = Position
-                    local SliderValue = math.floor(((Position.X.Scale * Max) / Max) * (Max - Min) + Min)
+                    local SliderValue = Precise and math.floor(((Position.X.Scale * Max) / Max) * (Max - Min) + Min) or ((Position.X.Scale * Max) / Max) * (Max - Min) + Min
 					GlobalSliderValue = SliderValue
                     Slider.Value.PlaceholderText = tostring(SliderValue)
                     Callback(GlobalSliderValue)
